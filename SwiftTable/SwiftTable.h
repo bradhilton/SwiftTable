6ec16a2a874483786077e7cb6650716ba3f8c035
bradhilton <brad.hilton.nw@gmail.com>
2016-02-12 15:23:25 -0700

    Initial Commit

---
+++ SwiftTable/SwiftTable.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTable.h
+//  SwiftTable
+//
+//  Created by Bradley Hilton on 2/12/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftTable.
+FOUNDATION_EXPORT double SwiftTableVersionNumber;
+
+//! Project version string for SwiftTable.
+FOUNDATION_EXPORT const unsigned char SwiftTableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTable/PublicHeader.h>
+
+
